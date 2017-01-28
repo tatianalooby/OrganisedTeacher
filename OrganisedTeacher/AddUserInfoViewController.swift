c4f8155e183031bf070d02a2d696c71276658696
tatianalooby <tatianalooby@gmail.com>
2017-01-28 20:23:14 +0300

    Added save and fetch core data

--- OrganisedTeacher/AddUserInfoViewController.swift
+++ OrganisedTeacher/AddUserInfoViewController.swift
@@ -8,24 +8,58 @@
 
 import UIKit
 
-class AddUserInfoViewController: UIViewController {
+class AddUserInfoViewController: UIViewController, UITextFieldDelegate {
     
     var connectingSegueID: String = ""
     
     @IBOutlet weak var teachingGroupTextField: UITextField!
     @IBOutlet weak var classroomNumberTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //Deactivate the save button until user types some info 
+        
+        teachingGroupTextField.delegate = self
+        classroomNumberTextField.delegate = self
+        
+        if (teachingGroupTextField.text?.isEmpty)! && (classroomNumberTextField.text?.isEmpty)! {
+            
+            saveButton.isUserInteractionEnabled = false
+        }
+
     }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        saveButton.isUserInteractionEnabled = true
+    }
+
 
     @IBAction func saveButtonTapped(_ sender: Any) {
         switch connectingSegueID {
         case "Day1VCToAddUserInfoVC":
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let day1TimeTableInfo = Day1TimeTableInfo(context: context)
+            day1TimeTableInfo.teachingGroupName = teachingGroupTextField.text
+            day1TimeTableInfo.classroomNumber = classroomNumberTextField.text
+            
+            //Save the data to core data
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
             print(connectingSegueID)
+            
         case "Day2VCToAddUserInfoVC":
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let day2TimeTableInfo = Day2TimeTableInfo(context: context)
+            day2TimeTableInfo.teachingGroupName = teachingGroupTextField.text
+            day2TimeTableInfo.classroomNumber = classroomNumberTextField.text
+            
+            //Save the data to core data
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
             print(connectingSegueID)
         default:
             print("default")
---
+++ OrganisedTeacher/PrototypeCellTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  PrototypeCellTableViewCell.swift
+//  OrganisedTeacher
+//
+//  Created by Tatiana Looby on 28/01/2017.
+//  Copyright © 2017 Tatiana Looby. All rights reserved.
+//
+
+import UIKit
+
+class PrototypeCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var day1TeachingGroupLabel: UILabel!
+    
+    @IBOutlet weak var day1ClassroomNumberLabel: UILabel!
+    
+    @IBOutlet weak var day2TeachingGroupLabel: UILabel!
+    
+    @IBOutlet weak var day2ClassroomNumberLabel: UILabel!
+}
